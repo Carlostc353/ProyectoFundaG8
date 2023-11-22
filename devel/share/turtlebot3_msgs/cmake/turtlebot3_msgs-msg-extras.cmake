9b04033e7851adfbe2d3aa4554985ca21fb7868b
Carlostc353 <carlos.torres@utec.edu.pe>
2023-11-22 00:34:37 -0500

    first

---
+++ devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(turtlebot3_msgs_MESSAGE_FILES "/home/ctorres/lab_ws/src/Turtlebot3/turtlebot3_msgs/msg/SensorState.msg;/home/ctorres/lab_ws/src/Turtlebot3/turtlebot3_msgs/msg/VersionInfo.msg;/home/ctorres/lab_ws/src/Turtlebot3/turtlebot3_msgs/msg/Sound.msg")
+set(turtlebot3_msgs_SERVICE_FILES "")
